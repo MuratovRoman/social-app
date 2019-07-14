eed34c70297639c3c9c45be3a8a2f8c9c87814ab
Roman Muratov <myratov.r@gmail.com>
2019-07-14 19:23:46 +0300

    finish post display logic

--- src/store.js
+++ src/store.js
@@ -4,10 +4,55 @@ const fb = require('./firebaseConfig.js')
 
 Vue.use(Vuex)
 
+// handle page reload
+fb.auth.onAuthStateChanged(user => {
+    if(user) {
+        store.commit('setCurrentUser', user)
+        store.dispatch('fetchUserProfile')
+
+        fb.usersCollection.doc(user.uid).onSnapshot(doc => {
+            store.commit('setUserProfile', doc.data())
+        })
+
+        //real time update from posts collection
+        fb.postsCollection.orderBy('createdOn', 'desc').onSnapshot(querySnapshot => {
+
+            //check if created by currentUser
+            let createdByCurrentUser
+            console.log(querySnapshot)
+            if (querySnapshot.docs.length) {
+                createdByCurrentUser = store.state.currentUser.uid == querySnapshot.docChanges()[0].doc.data().userId ? true : false
+            }
+
+            //add new posts to hiddenPosts array after initial load
+            if (querySnapshot.docChanges().length !== querySnapshot.docs.length 
+                && querySnapshot.docChanges()[0].type == 'added' && !createdByCurrentUser) {
+
+                    let post = querySnapshot.docChanges()[0].doc.data()
+                    post.id = querySnapshot.docChanges()[0].doc.id
+
+                    store.commit('setHiddenPosts', post)
+                } else {
+                    let postsArray = []
+
+                    querySnapshot.forEach(doc => {
+                        let post = doc.data()
+                        post.id = doc.id
+                        postsArray.push(post)
+                    })
+
+                    store.commit('setPosts', postsArray)
+                }
+        })
+    }
+})
+
 export const store = new Vuex.Store({
     state: {
         currentUser: null,
-        userProfile: {}
+        userProfile: {},
+        posts: [],
+        hiddenPosts: []
     },
     actions: {
         fetchUserProfile({ commit, state }) {
@@ -16,6 +61,10 @@ export const store = new Vuex.Store({
             }).catch(err => {
                 console.log(err)
             })
+        },
+        clearData({ commit }) {
+            commit('setCurrentUser', null)
+            commit('setUserProfile', {})
         }
     },
     mutations: {
@@ -24,6 +73,23 @@ export const store = new Vuex.Store({
         },
         setUserProfile(state, val) {
             state.userProfile = val
+        },
+        setPosts(state, val) {
+            if (val) {
+                state.posts = val
+            } else {
+                state.posts = []
+            }
+        },
+        setHiddenPosts(state, val) {
+            if (val) {
+                //make sure not to add duplicates
+                if (!state.hiddenPosts.some(x => x.id === val.id)) {
+                    state.hiddenPosts.unshift(val)
+                }
+            } else {
+                state.hiddenPosts = []
+            }
         }
     }
 })
